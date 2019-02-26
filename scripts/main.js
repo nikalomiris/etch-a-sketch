c70d7104fe903b499c217687a2eb5526f6f7126b
nikalomiris <nikalomiris@gmail.com>
2019-02-26 12:06:48 -0500

    Initial commit.

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let container = document.querySelector('.container');
+
+let button = document.querySelector('button');
+button.onclick = () => {
+    let size = prompt('Please enter the size of the board');
+    let boxDimensions = 600/size;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            let box = document.createElement('div');
+            box.setAttribute('style', 'float: left; border: solid 1px black; width: ' + boxDimensions + 'px; height: ' + boxDimensions + 'px; box-sizing: border-box;');
+            box.onmouseover = () => {
+                box.style.background = 'blue';
+            }
+            container.appendChild(box);
+        }
+    }
+}
