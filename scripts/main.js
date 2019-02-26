23410106580de85b68da68fa7190793aab85f41a
nikalomiris <nikalomiris@gmail.com>
2019-02-26 12:16:03 -0500

    The new game button clears the board.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ let container = document.querySelector('.container');
 
 let button = document.querySelector('button');
 button.onclick = () => {
+    container.innerHTML = '';
     let size = prompt('Please enter the size of the board');
     let boxDimensions = 600/size;
     for (i = 0; i < size; i++) {
